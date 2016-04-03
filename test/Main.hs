2ee51851e55b3904bd1c01ac6c4cc6cfbb748323
Tomas Vestelind <tomas.vestelind@gmail.com>
2016-04-03 19:20:04 +0200

    refactor unit tests

--- test/Main.hs
+++ test/Main.hs
@@ -9,7 +9,7 @@ import Data.List
 draw :: Spec
 draw = do
     describe "drawTickets" $ do
-        context "with x bought ticket" $ do
+        context "with x bought tickets" $ do
             it "draws exactly those tickets when x tickets are drawn" $ property $
                 \ps -> do
                     let ps' = map person ps
@@ -18,14 +18,14 @@ draw = do
                     if (length ps > 0)
                         then (sort <$> r) `shouldBe` (Just $ sort ps')
                         else r `shouldBe` Nothing
+            it "returns Nothing when drawing ticket x+1" $ property $
+                \a -> do
+                    let r = drawTickets (amount (a+1)) $ do
+                            replicateM_ a $ buyTickets $ person "Test"
+                    r `shouldBe` Nothing
         it "returns Nothing when 0 tickets are drawn" $ do
             let r = drawTickets (amount 0) $ buyTickets $ person "Test"
             r `shouldBe` Nothing
-        it "returns Nothing when tickets drawn > ticket bought" $ property $
-            \a -> do
-                let r = drawTickets (amount (a+1)) $ do
-                        replicateM_ a $ buyTickets $ person "Test"
-                r `shouldBe` Nothing
 
 lottery :: Spec
 lottery = describe "lottery" draw
